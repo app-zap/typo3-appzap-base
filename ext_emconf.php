01ef1d923928fb243af82015351be934151336b4
Sebastian Michaelsen <sebastian@michaelsen.io>
2015-04-27 10:31:24 +0200

    initial

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+/***************************************************************
+ * Extension Manager/Repository config file for ext "appzap_base".
+ ***************************************************************/
+$EM_CONF['appzap_base'] = array(
+	'title' => 'TYPO3 app zap base',
+	'description' => 'Common configuration',
+	'category' => 'be',
+	'state' => 'beta',
+	'author' => 'Sebastian Michaelsen',
+	'author_email' => 'sebastian@app-zap.de',
+	'author_company' => 'app zap',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-7.99.99',
+		),
+	),
+);
